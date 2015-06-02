45a27b450641bf43f499f5a6d639413e02d17fe6
Samuel Burer <samuel-burer@uiowa.edu>
2015-06-02 10:29:47 -0500

    Fix typo in Parade of Homes

--- 2015-05-26-Trader-Joes/main.R
+++ 2015-05-26-Trader-Joes/main.R
@@ -2,7 +2,6 @@
 
 library(stringr)
 library(ggmap)
-#library(pbapply)
 
 # Clear workspace
 
@@ -10,10 +9,12 @@ rm(list = ls())
 
 # Create helper functions
 
+# This is just so I don't have to type 'suppressWarnings'
 sW <- function(arg) {
     suppressWarnings(arg)
 }
 
+# This is just so the code doesn't fail on an error
 my.err.handler <- function(error) {
     error
 }
@@ -23,7 +24,7 @@ my.err.handler <- function(error) {
 
 if(!file.exists("Trader-Joes-Stores.pdf")) {
     download.file(url = "http://www.traderjoes.com/pdf/Trader-Joes-Stores.pdf",
-                  destfile = "Trader-Joes-Stores.pdf")
+        destfile = "Trader-Joes-Stores.pdf")
 }
 
 # Convert the PDF to text (if it hasn't already been converted). For
@@ -41,7 +42,8 @@ if(!file.exists("stores.Rdata")) {
 
     text <- sW(readLines("Trader-Joes-Stores.txt"))
 
-    # Create the "logical OR" list of state abbreviations
+    # Create the "logical OR" list of state abbreviations. Uses R's
+    # built-in state.abb character vector
 
     states <- paste(", ", state.abb, sep = "", collapse="|")
 
@@ -54,8 +56,9 @@ if(!file.exists("stores.Rdata")) {
     # Begin manual tweaks
 
     # The vast majority of addresses were on single lines, but a few
-    # were broken across two lines. Find those matches which require the
-    # preceding line. Each address should start with a number
+    # were broken across two lines. Key observation is that each address
+    # should start with a number. Find those matches which require the
+    # preceding line
 
     tmp <- sW(which(is.na(as.numeric(str_sub(text[matches], 1, 1)))))
     tmp <- matches[tmp]
@@ -65,7 +68,7 @@ if(!file.exists("stores.Rdata")) {
     text[tmp] <- paste(text[tmp-1], text[tmp], sep = ", ")
 
     # Fix one entry that had extra directions in address, which were
-    # messing up Google's ability to geocode the address
+    # messing up Google's ability to geocode it
 
     text[750] <- "6610 Marie Curie Dr, Elkridge, MD 21075"
 
@@ -107,7 +110,7 @@ if(!file.exists("stores.Rdata")) {
 
 limit <- 500
 
-# Find those stores that haven't been geocoded yet
+# Find the stores that haven't been geocoded yet
 
 wh <- which(is.na(df$lat) & is.na(df$lon) & is.na(df$address.google) &
     is.na(df$error))
@@ -124,7 +127,7 @@ if(length(wh) > 0) {
 
     for(ind in 1:length(wh)) {
 
-        # Call the geocode command
+        # Geocode the address
 
         coord <- tryCatch(
             geocode(df$address.tj[wh[ind]], output = "latlona"),
@@ -132,19 +135,24 @@ if(length(wh) > 0) {
             warning = my.err.handler
         )
 
-        # Error check
+        # If there was an error...
 
         if(is.null(coord$address) || is.na(coord$lat)) {
+
+            # Flag the error
+
             df$error[wh[ind]] <- TRUE
 
         # Otherwise, save the results
 
         } else {
+
             coord$address <- as.character(coord$address)
             df$lon[wh[ind]] <- coord$lon[1]
             df$lat[wh[ind]] <- coord$lat[1]
             df$address.google[wh[ind]] <- coord$address[1]
             df$error[wh[ind]] <- FALSE
+
         }
 
         # Write compressed CSV file
@@ -175,7 +183,8 @@ close(bz)
 
 save(df, file = "stores.Rdata")
 
-# Geocode the actual DC locations that I have found
+# Now geocode the actual DC locations that I found. Code is similar to
+# the above code
 
 if(file.exists("DCs.Rdata")) {
 
@@ -205,11 +214,11 @@ if(file.exists("DCs.Rdata")) {
 
 }
 
-# Build a first plot
+# Build a first plot of the USA
 
 p <- qmap("united states", zoom = 4, maptype = "roadmap", color = "bw")
 
-# Add the stores
+# Add the stores to the plot
 
 p <- p + geom_point(aes(x = lon, y = lat), data = df, alpha = I(0.5),
     color = I("blue"), size = 3)
@@ -224,21 +233,22 @@ centers <- as.data.frame(km$centers)
 names(centers) <- c("c.lon", "c.lat")
 subdf <- cbind(subdf, centers[km$cluster, ])
 
-# Add line segments between stores and centers
+# Add to the plot line segments between stores and centers
 
 p <- p + geom_segment(aes(x = lon, y = lat, xend = c.lon, yend = c.lat),
     data = subdf)
 
-# Add proposed DC locations
+# Add centers to the plot
 
-p <- p + geom_point(aes(x = c.lon, y = c.lat), data = centers,
-    color = I("red"), size = 4)
+p <- p + geom_point(aes(x = c.lon, y = c.lat), data = centers, alpha = I(0.75),
+    color = I("red"), size = 3)
 
 # Add true DC locations
 
-p <- p + geom_point(aes(x = lon, y = lat), data = DCs,
-    color = I("green"), size = 4)
+p <- p + geom_point(aes(x = lon, y = lat), data = DCs, 
+    color = I("green"), size = 3)
 
-# Save plot
+# Save plot (both low- and high-resolution)
 
+ggsave("Trader-Joes-Stores-web.png", p, dpi = 100, width = 5, height = 5)
 ggsave("Trader-Joes-Stores.png", p, dpi = 300, width = 5, height = 5)
